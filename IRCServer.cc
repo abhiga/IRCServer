1297f1321a8d744e26a8645fdbe59a3b8b710289
Gaurav <@purdue.edu>
2015-04-11 15:12:24 -0400

    Make IRCServer

--- IRCServer.cc
+++ IRCServer.cc
@@ -285,7 +285,7 @@ IRCServer::initialize()
 		while(fscanf(f, "%s", c) == 1) {
 			user = strtok(c, "|");
 			pass = strtok(NULL,"|");
-			printf("%d%d",strcmp(user,"abhiga"),strcmp(pass,"abhig"));
+			printf("%d%d\n",strcmp(user,"abhiga"),strcmp(pass,"abhig"));
 			Users.insertItem(user, pass);
 		}
 	}
