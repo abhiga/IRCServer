4d4e526f0a3fa24b6cc3e313ed730b6952fb3294
Gaurav <@purdue.edu>
2015-04-12 01:06:26 -0400

    Make IRCServer

--- IRCServer.cc
+++ IRCServer.cc
@@ -438,6 +438,7 @@ IRCServer::getAllUsers(int fd, const char * user, const char * password,const  c
 	char *ptr;
 	void* pass;
 	char *msg;
+	checkPassword(fd, user, password);
 	if(checkPassword(fd, user, password)) {
 		HashTableVoidIterator iterator(&Users);
 		while(iterator.next(ch,pass)) {
