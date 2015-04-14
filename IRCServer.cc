44e78da61ca9e61023573829e97d1af4002363e9
Gaurav <@purdue.edu>
2015-04-14 15:40:11 -0400

    Make IRCServer

--- IRCServer.cc
+++ IRCServer.cc
@@ -554,7 +554,7 @@ IRCServer::getAllUsers(int fd, const char * user, const char * password,const  c
 		const char * msg = "DENIED\r\n";
 		write(fd,msg,strlen(msg));
 	}
-	write(fd,"\n",strlen("\n"));
+	write(fd,"\r",strlen("\r"));
 
 }
 
