d232029520c947baded002cced14463f5d1785e9
Gaurav <@purdue.edu>
2015-04-17 19:10:22 -0400

    Make IRCServer

--- IRCServer.cc
+++ IRCServer.cc
@@ -333,7 +333,7 @@ IRCServer::checkPassword(int fd, const char * user, const char * password) {
 IRCServer::addUser(int fd, const char * user, const char * password, const char * args)
 {
 	// Here add a new user. For now always return OK.
-	char *store = (char*) malloc(50*sizeof(char));
+	char *store = (char*) malloc(100*sizeof(char));
 	FILE *file = fopen("password.txt", "a");
 	//store = strdup(user);
 	//memcpy(store, user, 100*sizeof(char));
